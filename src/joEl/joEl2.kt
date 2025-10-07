6b55bc72462143ac2bb4273619e9c6174531a6c6
ezequiel.carracedo.7e9 <ezequiel.carracedo.7e9@itb.cat>
2025-10-07 13:52:01 +0200

    v5

--- src/joEl/joEl2.kt
+++ src/joEl/joEl2.kt
@@ -11,39 +11,30 @@ fun main() {
     // Demanem les voltes
 
     var voltes: Int = scan.nextInt();
-    var cont: Int = 0;
-    var sumaSenars: Int = 1;
+    var sumaSenars: Int = 0;
     var sumaParell: Int = 0;
-    var contParell: Int = 2;
-    var parellActual: Int = 0;
 
 
     // Fem el bucle per sumar els parells i senars.
-    while (voltes < cont) {
+    repeat(voltes) {
         var num: Int = scan.nextInt();
-        sumaParell = num;
-        parellActual = num;
 
-        cont++;
-        while (sumaParell <= num) {
-            parellActual = parellActual - 2;
-            sumaParell = sumaParell + (parellActual)
-            contParell = contParell + 2;
+        // Comprovem si és parell o senar.
+        for (i in 1..num) {
+            if (i % 2 == 0) {
+                sumaParell += i;
+            } else {
+                sumaSenars += i;
+            }
 
         }
-        while (sumaSenars <= (num - 1)) {
-            sumaSenars + 2;
-        }
-
-
+        println("PARELLS: $sumaParell SENARS: $sumaSenars")
     }
-    println("PARELLS: $sumaParell SENARS: $sumaSenars")
+    //Imprimim el resultat.
 
 
     scan.close();
 
 }
-
-// Imprimim el resultat.
 
 
