7d2ea57572b083e29fd80342fe6c0529d61569ea
zequi-itb <ezequiel.carracedo.7e9@itb.cat>
2025-10-09 10:45:14 +0200

    v4.1

--- src/joEl/joEl.kt
+++ src/joEl/joEl.kt
@@ -1,39 +1,29 @@
 import java.util.Scanner
 
-// Quan Bambino va sortir amb el seu primer ex, l'ex tenia 18 anys.
-// Bambino va dir "mai més ningú de 18 anys" El segón ex de bambino tenia 29.
-// Bambino va dir "mai més ningú de 29 anys" El tercer tenia 26. Bambino... sí, ja us ho imagineu.
-
-//Per cada cas de prova s'ha de dir SI si l'interés no té l'edat dels exs de bambino i NO en cas contrari.
+// Isabel Preysler t’ha demanat fer una torre de Ferrero Rocher per a la recepció de l’ambaixador. La torre ha de ser en forma de piràmide quadrada.
+// Això significa que el primer pis tindrà un Ferrero, el segon 4, el tercer 9, el quart 16, i així succesivament (cada pis és un quadrat amb un més a cada cantó)
+//Per cada cas de prova caldrà respondre: El nombre de bombons necessaris per a construir la piràmide
+// (no els bombons del pis que fa de base, sinó tots els bombons que fan falta per fer la piràmide).
 
 fun main() {
-    val SCAN: Scanner = Scanner(System.`in`);
+    val scan: Scanner = Scanner(System.`in`);
 
-    // Demanem les dades.
-    // Evito utilitzar la paraula "ex" per les variables, ja que em surten moltes paraules clau de kotlin que comencen amb "ex"
-    var parellaBambino1: Int = SCAN.nextInt();
-    var parellaBambino2: Int = SCAN.nextInt();
-    var parellaBambino3: Int = SCAN.nextInt();
-    var interesActual: Int = SCAN.nextInt();
-    var repeteix: Boolean = false;
+    // Demanem el numero de casos a considerar.
+    var voltes: Int = scan.nextInt();
+    var suma: Int = 0;
 
-    // Comparem l'edat dels ex de Bambino amb el seu últim interès
-    when (interesActual) {
-        parellaBambino1 -> repeteix = true;
-        parellaBambino2 -> repeteix = true;
-        parellaBambino3 -> repeteix = true
-        else -> repeteix = false
+    //Fem un bucle per demanar els números i calcular el nombre de bombons.
+    repeat(voltes) {
+        var num: Int = scan.nextInt();
+
+        // Fem una iteracio numero per numero per calcular el total de bombons.
+        for (i in 1..num) {
+            suma += i * i;
+        }
+        //Imprimim el resultat
+        println(suma)
     }
-
-    // Imprimim el resultat
-    if (repeteix) {
-        println("NO")
-    } else {
-        println("SI")
-    }
-
-
-    SCAN.close()
+    scan.close()
 }
 
 
