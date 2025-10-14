208a64bcdb5fe4155823728559e1da185543f49c
ezequiel.carracedo.7e9 <ezequiel.carracedo.7e9@itb.cat>
2025-10-14 12:14:29 +0200

    v5

--- src/joEl/joEl2.kt
+++ src/joEl/joEl2.kt
@@ -1,27 +1,14 @@
 import java.util.Scanner
 
-// Un icónic atac de mag en el WoW es Explosión Arcana.
-// Aquest atac comença fent poc mal, pero cada cop que el fas servir fa un 100% més de mal.
-// Per exemple, si el primer cop fa 100 punts de mal, el següent farà 200, el següent 300, el següent 400, i així fins que passa un temps.
+// Ets un rastrejador i estàs analitzant els casos de covid de tots els barris de catalunya.
+// Et passaran els valors diaris de l'índex de contagis. La teva única missió és avisar si algun valor passa de 300.
 
 fun main() {
     val scan: Scanner = Scanner(System.`in`);
 
     // Demanem les dades
-    var dany: Int = scan.nextInt();
-    var vidaEnemic: Int = scan.nextInt();
 
-    // Declarem variables necessaries per fer comptabilitzar els cops.
-    var cops: Int = 1;
 
-    // Calculem quants cops fan falta per matar a l'enemic.
-    while (vidaEnemic > 0) {
-        vidaEnemic -= dany*cops
-        cops++;
-    }
-
-// Imprimim la quantitat de cops.
-    println(cops)
 
     scan.close()
 }
