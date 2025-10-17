a7ce06a43b99a0742b23277a0d6d72b5bf807d85
zequi-itb <ezequiel.carracedo.7e9@itb.cat>
2025-10-17 10:45:11 +0200

    v4.1

--- src/joEl/joEl.kt
+++ src/joEl/joEl.kt
@@ -1,30 +1,35 @@
 import java.util.Scanner
 
-// Un icónic atac de mag en el WoW es Explosión Arcana.
-// Aquest atac comença fent poc mal, pero cada cop que el fas servir fa un 100% més de mal.
-// Per exemple, si el primer cop fa 100 punts de mal, el següent farà 200, el següent 300, el següent 400, i així fins que passa un temps.
+// El tió és un tronc màgic que ens caga regals la nit de Nadal mentre l'apallisem a cops de bastó i cantem la cançoneta
+// "Caga tió! Tió de Nadal No caguis arengades que son salades..."
+//La cançó en qüestió ha tret protagonisme al propi tió de tal forma que se li ha començat a dir "cagatió" donant peu a que sorgeixi la polèmica.
+// I tu com li dius, "tió" o "cagatió"?
+//El nom correcte és "tió" i, si no aturem aquesta tendència, la propera generació cantarà "Caga cagatió!..."
+// i la seguent li dirà "cagacagatió" i la següent cantarà "Caga cagacagatió!...", amb la qual cosa és molt probable que es perdi la tradició perquè ningú no entendrà res.
+
+// Per a cada cas cal indicar el primer vers de la cançó que farà servir aquella generació per fer cagar el tió.
+// Per evitar problemes no farem servir tildes i escriurem "tio" en comptes de "tió".
 
 fun main() {
     val scan: Scanner = Scanner(System.`in`);
 
-    // Demanem les dades
-    var danyInicial: Int = scan.nextInt();
-    var vidaEnemic: Int = scan.nextInt();
+    // Demanem la quantitat de casos a evaluar.
+    var casos: Int = scan.nextInt();
 
-    // Declarem variables necessaries per fer comptabilitzar els cops.
-    var danyIncrement: Int = 0;
-    var sumaTotalDany: Int = 0;
-    var cops: Int = 0;
+    // Fem un bucle per demanar les generacions segons la quantitat de casos.
+    repeat(casos) {
+        var generacions: Int = scan.nextInt();
+        var cagaTio: String = "Caga ";
 
-    // Calculem quants cops fan falta per matar a l'enemic.
-    while (sumaTotalDany < vidaEnemic) {
-        danyIncrement = danyIncrement + ((danyInicial * 100) / 100)
-        sumaTotalDany += danyIncrement;
-        cops++;
+        // Fem un bucle que sumi de 2 en 2 per afegir la paraula "caga" a la string.
+        // Començem des-de el 3 perque en 1 i 2 no fa falta que afageixi res a la string.
+        for (i in 3..generacions step 2) {
+            cagaTio += "caga"
+        }
+
+        // Imprimim el resultat
+        println(cagaTio + "tio!")
     }
-
-// Imprimim la quantitat de cops.
-    println(cops)
 
     scan.close()
 }
--- src/joEl/kotlin.kt
+++ src/joEl/kotlin.kt
@@ -1,39 +1,81 @@
 import java.util.Scanner
 
-//Fes un programa que llegeixi una frase. La sortida ens diu la quantitat de vegades que apareix cada vocal (tant majúscula com minúscula).
-//Per cada cas de prova caldrà respondre: A: xxx E: xxx I: xxx O: xxx U: xxx
+//> (problema NO patrocinado por Square Enix)
+// ¿Has oído hablar del famoso MMORPG aclamado por la crítica: FINAL FANTASY XIV?
+// Si es así, enhorabuena, seguro que tu vida es mejor que la de los que no lo han hecho. Si no es así... ¿A qué esperas?
+// Sea como sea seguro que SÍ que conoces la famosa cancioncilla que dice:
+// "Un elefante se balanceaba sobre la tela de una araña..." pues verás, en FINAL FANTASY XIV existe una adorable raza de pequeños humanoides
+// que ¡Adora balancearse! El problema es que las arañas les aterran, pero como son muy temerarios, si ven una telaraña intentarán balancearse...
+// Aunque siempre respetando la capacidad de la tela, no queremos ningún accidente.
+
+// La frase "Hay x telarañas llenas." donde x es el número de telarañas COMPLETAMENTE OCUPADAS.
+// Ten en cuenta que una telaraña solo quedará ocupada cuando en la secuencia aparezcan tantos Lalafel como los necesarios para ocuparla.
+// También piensa que NO QUEREMOS ACCIDENTES por lo que siempre que haya una telaraña completa, los Lalafels que no quepan esperaran a que aparezca otra telaraña.
+// Por último recuerda: LOS LALAFELS ODIAN LAS ARAÑAS Si aparece una araña en la secuencia, todos los lalafels que estaban balanceándose
+// se irán corriendo y las telas se romperán, quedando inutilizables.
 
 fun main() {
     val scan: Scanner = Scanner(System.`in`);
 
     // Demanem la quantitat de casos a evaluar.
     var casos: Int = scan.nextInt();
-    scan.nextLine();
+    scan.nextLine()
 
-    // Demanem les dades a evaluar.
+    // Fem un bucle amb la quantitat de casos a evaluar i demanem la secuencia de paraules i la capacitat de la telearanya.
     repeat(casos) {
-        var frase: String = scan.nextLine().lowercase();
 
-        // Declarem variables per comptabilitzar la quantitat de cadascuna de les vocals que hi ha a la frase.
-        var vocalA: Int = 0
-        var vocalE: Int = 0
-        var vocalI: Int = 0
-        var vocalO: Int = 0
-        var vocalU: Int = 0
+        var contadorLalafels: Int = 0;
+        var contadorTelaranyes: Int = 0;
+        var hiHaAranya: Boolean = false;
+        var telaranyesPlenes: Int = 0;
+        do {
+            var paraula: String = scan.next().lowercase();
 
-        // Fem un bucle "for" per fer una iteracio de cada posicio de la frase i amb un "when" comprovar quantas vocals hi ha de cadascuna
-        for (i in 0..frase.length - 1) {
-            when (frase.get(i)) {
-                'a' -> vocalA++
-                'e' -> vocalE++
-                'i' -> vocalI++
-                'o' -> vocalO++
-                'u' -> vocalU++;
+
+            var teComa: Boolean = paraula.get(paraula.length - 1) == ','
+
+            // Comprovem si la paraula te coma per saber si restar-li o no l'ultim caracter
+            if (teComa) {
+                paraula = paraula.dropLast(1)
             }
+
+            //// Verifiquem si apareix la paraula aranya en la secuencia per imprimim directament que hi ha 0 telaranyes plenes.
+            if (paraula != "aranya") {
+
+                //Comptabilitzem quants Lalafels i telaranyes hi ha.
+                when (paraula) {
+                    "lalafel" -> contadorLalafels++;
+                    "telaranya" -> contadorTelaranyes++
+                }
+
+
+                //Imprimim el resultat si apareix alguna aranya.
+            } else {
+                hiHaAranya = true;
+            }
+
+
+        } while (teComa)
+
+        var capacitat: Int = scan.nextInt();
+        scan.nextLine();
+
+        telaranyesPlenes = contadorLalafels / capacitat
+        if (hiHaAranya) {
+            println("Hay 0 telaranyas llenas.")
+        } else {
+            println("Hay $telaranyesPlenes telaranyas llenas.")
         }
-        // Imprimim el resultat.
-        println("A: $vocalA E: $vocalE I: $vocalI O: $vocalO U: $vocalU")
+
+
     }
+
+
+    // Multipliquem la quantitat de telaranyes per la capacitat
+
+    // Mirem si la quantitat de lalafels es mes igual o menor que la quantitat de telaranyes
+
+    // Evaluem si hi caben totes en una o en mes.
 
     scan.close()
 }
