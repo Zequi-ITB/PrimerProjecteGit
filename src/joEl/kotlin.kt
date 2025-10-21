69b4de16acb0209c8fc10afa6e2ec7469f86e24c
ezequiel.carracedo.7e9 <ezequiel.carracedo.7e9@itb.cat>
2025-10-21 10:43:25 +0200

    21/10

--- src/joEl/kotlin.kt
+++ src/joEl/kotlin.kt
@@ -1,80 +1,45 @@
 import java.util.Scanner
 
-//> (problema NO patrocinado por Square Enix)
-// ¿Has oído hablar del famoso MMORPG aclamado por la crítica: FINAL FANTASY XIV?
-// Si es así, enhorabuena, seguro que tu vida es mejor que la de los que no lo han hecho. Si no es así... ¿A qué esperas?
-// Sea como sea seguro que SÍ que conoces la famosa cancioncilla que dice:
-// "Un elefante se balanceaba sobre la tela de una araña..." pues verás, en FINAL FANTASY XIV existe una adorable raza de pequeños humanoides
-// que ¡Adora balancearse! El problema es que las arañas les aterran, pero como son muy temerarios, si ven una telaraña intentarán balancearse...
-// Aunque siempre respetando la capacidad de la tela, no queremos ningún accidente.
+// Un eclipsi solar es produeix quan la lluna s'interposa al camí de la llum del sol i projecta la seva ombra a la Terra.
+//Aquest eclipsi total es produeix aproximadament cada any i mig en algun lloc de la Terra. Un eclipsi parcial, quan la lluna no recobreix completament el sol, es produeix almenys dues vegades per any, en algun lloc de la Terra.
+//Acordem que per aquest problema:
+//1 any: 365 dies
+//Eclipsi Parcial cada ½ any: 183 dies (per fer-ho rodó)
+//Eclipsi Total cada 1 any i mig: 548 dies
 
-// La frase "Hay x telarañas llenas." donde x es el número de telarañas COMPLETAMENTE OCUPADAS.
-// Ten en cuenta que una telaraña solo quedará ocupada cuando en la secuencia aparezcan tantos Lalafel como los necesarios para ocuparla.
-// También piensa que NO QUEREMOS ACCIDENTES por lo que siempre que haya una telaraña completa, los Lalafels que no quepan esperaran a que aparezca otra telaraña.
-// Por último recuerda: LOS LALAFELS ODIAN LAS ARAÑAS Si aparece una araña en la secuencia, todos los lalafels que estaban balanceándose
-// se irán corriendo y las telas se romperán, quedando inutilizables.
 
 fun main() {
     val scan: Scanner = Scanner(System.`in`);
 
     // Demanem la quantitat de casos a evaluar.
     var casos: Int = scan.nextInt();
-    scan.nextLine()
 
-    // Fem un bucle amb la quantitat de casos a evaluar i demanem la secuencia de paraules i la capacitat de la telearanya.
+    // Declarem variables per fer calculs i de control.
+    var diesEclipsiTotal: Int = 548;
+    var diesEclipsiParcial: Int = 183;
+    var eclipsiTotal: Boolean = false;
+
+    // Fem un bucle amb la quantitat de casos a evaluar per demanar les dades al usuari.
     repeat(casos) {
-
-        //Declarem variables per comptabilitzar els lalafels i les telaranyes.
-        var contadorLalafels: Int = 0;
-        var contadorTelaranyes: Int = 0;
-        var telaranyesPlenes: Int = 0;
-
-        // Fem un bucle do-while per demanar les frases i fer diferents comprovacions
-        do {
-            var paraula: String = scan.next().lowercase();   //Utilitzem lowercase per evaluar totes les entrades de igual manera.
-
-            // Declarem una variable per saber si la paraula te coma al final, aixi sabrem si es l'ultima quan no tingui coma.
-            var teComa: Boolean = paraula.get(paraula.length - 1) == ','
-
-            // Comprovem si la paraula te coma per saber si restar-li o no l'ultim caracter
-            if (teComa) {
-                paraula = paraula.dropLast(1)
-            }
-
-            //// Verifiquem si apareix la paraula aranya en la secuencia per resetear els comptadors a 0.
-            if (paraula == "aranya") {
-                contadorLalafels = 0;
-                contadorTelaranyes = 0;
-            }
-
-            //Comptabilitzem quants Lalafels i telaranyes hi ha.
-            when (paraula) {
-                "lalafel" -> contadorLalafels++;
-                "telaranya" -> contadorTelaranyes++
-            }
-
-
-        } while (teComa)
-
-        // Demanem le capacitat de la telaranya.
-        var capacitat: Int = scan.nextInt();
-        scan.nextLine();
-
-        // Comprovem si la capacitat es 0, de no ser aixi, fem el calcul de quantes telaranyes hi ha plenes i imprimim el resultat.
-        if (capacitat <= 0) {
-            println("Hay 0 telaranyas llenas.")
-        }
-
-        // Dividim els lalafels per la capacitat per saber quantes telaranyes fan falta per tots.
-        // Si el resultat es igual o major a la quantitat de telaranyes imprimim el resultat.
-        else
-            if (contadorLalafels / capacitat >= contadorTelaranyes) {
-                telaranyesPlenes = contadorTelaranyes
-                println("Hay $telaranyesPlenes telaranyas llenas.")
+        var dies: Int = scan.nextInt();
+        var diesRestants: Int = 0;
+        if (dies % 548 == 0) {
+            println("TOTAL "+diesRestants)
+        } else if (dies % 183 == 0) {
+            println("PARCIAL "+ diesRestants)
+        } else {
+            if ((diesEclipsiTotal - dies % diesEclipsiTotal) < (diesEclipsiParcial - dies % diesEclipsiParcial)) {
+                diesRestants = diesEclipsiTotal - (dies % diesEclipsiTotal)
             } else {
-                telaranyesPlenes = contadorLalafels / capacitat;
-                println("Hay $telaranyesPlenes telaranyas llenas.")
+                diesRestants = diesEclipsiParcial - (dies % diesEclipsiParcial)
+
             }
+            if (eclipsiTotal) {
+                println("NO TOTAL $diesRestants")
+            } else {
+                println("NO PARCIAL $diesRestants")
+            }
+        }
     }
 
     scan.close()
