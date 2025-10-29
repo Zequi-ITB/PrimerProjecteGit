d0d60bd9a950bdc2528a29e84489b9b9f28265f9
ezequiel.carracedo.7e9 <ezequiel.carracedo.7e9@itb.cat>
2025-10-29 10:44:46 +0100

    29/10

--- src/joEl/kotlin.kt
+++ src/joEl/kotlin.kt
@@ -1,40 +1,76 @@
 import java.util.*
 
-//Abans de començar a fer exercicis d'arrays, comprovem que podem llegir i escriure bé en arrays.
-//El problema serà de tres línies. La primera tindrà un nombre, K
-// que et dirà el tamany de l'array que has de crear La segona son K
-// nombres, separats per espais que et dirà el contingut de l'array que has de llegir. La tercera serà N
-//, una posició de l'array a on has d'accedir. Es garanteix que N<K
-
-//Tornaràs dues linies. La primera serà l'array sencer escrit, amb espais entre cada caràcter. La segona serà el contingut de l'array a la posició N
-// (recorda que la primera posició es 0).
-
+//Un professor vol saber per un treball quin es el nen més alt de la seva classe.
+//El programa escriurà “Nen Nº“ del nen més alt. En cas que tots superin l'altura màxima escriurà "No hi ha cap nen".
 fun main() {
     val scan: Scanner = Scanner(System.`in`);
 
-    // Demanem el tamany de l'array.
-    var tamany: Int = scan.nextInt();
+    // Demanem l'altura maxima per ser considerat nen.
+    var alturaMax: Int = scan.nextInt()
 
-    // Creem l'array
-    var llistatDeNumeros = arrayOfNulls<Int>(tamany);
+    // Demanem les altures de les persones a considerar.
+    var altura1: Int = scan.nextInt()
+    var altura2: Int = scan.nextInt()
+    var altura3: Int = scan.nextInt()
 
-    // Fem un bucle amb les voltes segons el tamany del array i demanem les dades que tindra l'array en cada posicio
-    for (i in llistatDeNumeros.indices) {
-        llistatDeNumeros[i] = scan.nextInt();
+    // Declarem variables per comprovar si s'ha superat l'altura maxima.
+    var altura1Superada: Boolean = false;
+    var altura2Superada: Boolean = false;
+    var altura3Superada: Boolean = false;
+    var dosAlturaSuperada: Boolean = false;
+
+    // Comprovem si s'ha superat l'altura maxima en una o mes d'una persona.
+    if (altura1 > alturaMax) {
+        altura1Superada = true
+    }
+    if (altura2 > alturaMax) {
+        altura2Superada = true
+        if (altura1Superada) {
+            dosAlturaSuperada = true
+        }
+    }
+    if (altura3 > alturaMax) {
+        altura3Superada = true
+        if (altura1Superada || altura2Superada) {
+            dosAlturaSuperada = true
+        }
     }
 
-    // Demanem quina posicio volem llegir
-    var posicioImprimir: Int = scan.nextInt();
+    // Declarem variables per comprovar quin es el mes alt
+    var mesAlt: Int = 0
+    var guanyador: Int = 0
 
-    // Imprimim el array sencer(Imprimim un espai si no es el primer element) i la posicio consultada.
-    for (i in llistatDeNumeros.indices)
-        if (llistatDeNumeros[i] != 0) {
-            print(" ")
+    // Fem una condicio per si els tres superan l'altura maxima i imprimim el missatge corresponent
+    if (altura1Superada && altura2Superada && altura3Superada) {
+        println("No hi ha cap nen")
+    } else {
+        // Fem tota la serie de comprobacions en el cas de que mes de una persona superi l'altura o solo una.
+        if (dosAlturaSuperada) {
+            if (altura1Superada && altura2Superada) {
+                mesAlt = altura3
+            } else if (altura1Superada && altura3Superada) {
+                mesAlt = altura2
+            } else if (altura2Superada && altura3Superada) {
+                mesAlt = altura1
+            }
+        } else if (altura1Superada) {
+            mesAlt = maxOf(altura2, altura3)
+        } else if (altura2Superada) {
+            mesAlt = maxOf(altura3, altura1)
+        } else if (altura3Superada) {
+            mesAlt = maxOf(altura1, altura2)
+        } else {
+            mesAlt = maxOf(altura1, altura2, altura3)
         }
-    print(llistatDeNumeros[i])
-    println()
-    println(llistatDeNumeros[posicioImprimir])
 
-
+        // Asignem el numero de posicio segons el que tingui l'altura mes gran i imprimim el resultat.
+        when (mesAlt) {
+            altura1 -> guanyador = 1
+            altura2 -> guanyador = 2
+            altura3 -> guanyador = 3
+        }
+        println("Nen $guanyador")
+    }
     scan.close()
+
 }
