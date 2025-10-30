cf7ddde047d0cafe86be9de8eaa3dc890ad28b83
zequi-itb <ezequiel.carracedo.7e9@itb.cat>
2025-10-30 08:01:41 +0100

    v4.1

---
+++ src/joEl/joel.java
@@ -0,0 +1,12 @@
+package joEl;
+
+public class joel {
+    static void main(String[] args) {
+        int perro = 1;
+        switch (perro){
+            case 5 : System.out.println("");
+            break;
+        }
+
+    }
+}
