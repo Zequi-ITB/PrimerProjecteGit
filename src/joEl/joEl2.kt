cc8e6793320608e8f9304a0c9e2970a8a028206d
ezequiel.carracedo.7e9 <ezequiel.carracedo.7e9@itb.cat>
2025-10-31 13:53:24 +0100

    31/10

--- src/joEl/joEl2.kt
+++ src/joEl/joEl2.kt
@@ -1,56 +1,61 @@
 import java.util.Scanner
-import kotlin.math.absoluteValue
 
-//A Bambino li agrada jugar al Parchís, i el grup de música Parchis.
-// Li agrada especialment jugar al Parchís mentre escolta Parchís. A aquest concepte li diu ParchísParchís.
-//En el Parchis, quan arribes a “casa” (la zona del teu color a on només poden entrar les teves fitxes),
-// has d’arribar a l'última casella amb un nombre exacte de jugades, si no “rebotes” en la casella de meta i tornes enrera.
-// La següent tirada tornes a anar endavant
+//Havien passat més de 35 anys des que va acabar l’antiga EGB (Educació General Bàsica),
+// però no importava. Semblava que fos ahir. L’alumnat del Colegio Nacional Prim
+// es tornava a reunir per saber de la seva vida. Era una d’aquelles coses que val la pena viure.
+// Per mantenir l’esperit i il·lusió entre tots aquells nens i nenes que rondaven la cinquantena, Celia,
+// una de les alumnes més actives, va fer una proposta que a tothom li va semblar be: compraria un número
+// de la loteria de la Grossa i el jugaríem entre tots. Be, aquesta era la idea, i la van fer, però no tothom
+// tenia interès en participar o be ho volien fer en quantitats diferents. Van buscar un número especial
+// per ells i Celia va comprar tota la quantitat possible d’aquest número. Després, va obrir una llista de peticions,
+// on cada company podia demanar un import que després li abonaria. No estava clar si podria satisfer totes les peticions
+// o si al final seria ella qui s’hauria de quedar el sobrant no assignat.
 
+//Celia no te en compte l’ordre d’arribada de les sol·licituds.
+//La seva intenció és satisfer quantes més sol·licituds, millor. Això sí, una sol·licitud no es
+//pot quedar a mitjes ... o s’accepta completament, o es rebutja. Per cada cas de prova, caldrà respondre:
+//Una sola línia amb un valor que indicarà el número de sol·licituds acceptades (el zero no compta)
 
-//Per a cada cas hauràs de dir la posició final després de les 3 tirades de daus, tenint en compte de que si algún
-// cop arrives a l’última casella (8) ja has guanyat i la resta de tirades son irrelevants
 fun main() {
     val scan: Scanner = Scanner(System.`in`);
 
     // Demanem els casos a considerar
     var casos: Int = scan.nextInt();
-    val TAMANY_ARRAY = 3
-    var POSICIO_META_FINAL = 8
+
+    scan.nextLine()
+
+
+    // prodria agafar tots els elements com un string i pasarlos a un array despres
+
 
     // Fem un bucle amb la quantitat de casos
     repeat(casos) {
-        var ultimaCasella: Boolean = false
+        var contadorElementos: Int = 0
+        var importNumero: Int = scan.nextInt()
+        var cadena: String =""
+       do {
+           cadena = scan.next()
+           contadorElementos++;
 
-        // Creem l'array i demanem la primera posicio i les jugades
-        var posicio: Int = scan.nextInt()
-        var llistatDeJugadas: Array<Int> = Array(TAMANY_ARRAY) { scan.nextInt() }
+       }while (cadena!="0")
 
-        // Fem els calculs de les jugades
-        for (i in llistatDeJugadas.indices) {
-            posicio = ((llistatDeJugadas[i] + posicio) - POSICIO_META_FINAL).absoluteValue
-            if (posicio != 0) {
-                posicio = POSICIO_META_FINAL - posicio
-            }
 
-            if (posicio == 0) {
-                ultimaCasella = true
-            }
+       // Creem l'array
+
+
+
+        var llistatDeSolicituds: arr = (contadorElementos){0}
+
+        llistatDeSolicituds = cadena.split(" ").map { it.toInt() }
+
+        for (i in llistatDeSolicituds.indices){
+
+            llistatDeSolicituds[i]= cadena[i].digitToInt()
+            println(llistatDeSolicituds[i])
         }
-        //Imprimim els resultats.
-        if (ultimaCasella) {
-            println("8")
-        } else {
-            when (posicio) {
-                7 -> println("7")
-                6 -> println("6")
-                5 -> println("5")
-                4 -> println("4")
-                3 -> println("3")
-                2 -> println("2")
-                1 -> println("1")
-            }
-        }
+
+
+
 
     }
     scan.close()
