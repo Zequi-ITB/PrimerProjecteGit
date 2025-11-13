35cb04dc3a19abac793c66dbec33240e6a1bed7e
zequi-itb <ezequiel.carracedo.7e9@itb.cat>
2025-11-13 10:43:54 +0100

    13/11

--- src/joEl/joEl2.kt
+++ src/joEl/joEl2.kt
@@ -1,43 +1,91 @@
 import java.util.Scanner
 
-//Francesco Virgolini, el cotxe més veloç de tutti l'Italia, ha avançat a un altre cotxe en la copa del món.
-//La primera línia indica els casos de prova a considerar Cada cas compta amb un nombre variable de línies.
-// La primera líniea té un enter, K que indica quants cotxes hi ha a la cursa, i després vindràn K
-// línies, cadascuna amb el nom d'un cotxe. Es garanteix que un dels cotxes es dira "Francesco Virgolini", i mai serà el primer.
+//Alecs té dues aficions. Queixar-se i col·leccionar Pokèmon. No obstant, la seva col·lecció es tan gran que s’ha quedat
+//sense espai al Banc Pokèmon. Per això, farà una neteja de tots els seus Pokèmon. Vol quedar-se amb només un Pokèmon de cada tipus
+//(és a dir, no en vol de repetits), però vol quedar-se amb el que sigui de més nivell. Els Pokèmon tenen dues dades per a identificar-se.
+//El seu nom a la Pokèdex, un nombre que va del 1 al 700, i el seu nivell, que va del 1 al 100. L’objectiu es comparar els Pokèmon i quedar-se amb el de més nivell,
+//per tal de que no quedin de repetits.
 
-//Per cada cas de prova es vol retornar la llista amb Francesco Virgolini havent adelantat una posició, en format estàndard de llista.
+//El programa haura de donar una linea de sortida, amb tots els Pokèmon (mostrant el seu nombre a la Pokèdex)
+//que s’han quedat al banc, separats per un espai, amb un espai al final. És molt important que els Pokèmon han d’estar
+//en el ordre en que estaven a l’entrada, sent eliminats els de nivell més baix’.
+
 
 fun main() {
     val scan: Scanner = Scanner(System.`in`)
 
     // Demanem la quantitat de casos
     var casos: Int = scan.nextInt()
+    // Demanem el tamany dels llistats
+    var tamany: Int = scan.nextInt()
+
 
     // Fem un bucle amb la quantitat de casos
     repeat(casos) {
 
-        // Demanem el tamany de la llista
-        var tamany: Int = scan.nextInt()
-        scan.nextLine()
+        // Definim el llistat de pokemons i el de nivells i llegim els valor per teclat
+        var llistatPokemons: MutableList<Int> = MutableList<Int>(tamany) { scan.nextInt() }
+        var llistatNivells: MutableList<Int> = MutableList<Int>(tamany) { scan.nextInt() }
+        var llistatPokemons_Nou: MutableList<Int> = mutableListOf()
+        var posicioRepetit_last: Int = 0
+        var posicioRepetitFirst: Int = 0
 
-        // Definim la llista i llegim els valor per teclat
-        var llistatCotxes: MutableList<String> = MutableList<String>(tamany) { scan.nextLine() }
+        // Fem una iteracio i anem buscant coincidencies de cada pokemon
+        for (i in llistatPokemons.indices) {
 
-        //Declarem variables per guardar el nom del cotxe que reemplaçarem i per guardar la posicio en la que trobem a francesco
-        var cotxe_reemplazar: String = ""
-        var posicioFrancesco: Int = 0
+            //Mirem si l'element és dins de la llista.
+            if (llistatPokemons[i] in llistatPokemons) {
 
-        // Busquem a francesco i obtenim la seva posicio amb indexOf
-        posicioFrancesco = llistatCotxes.indexOf("Francesco Virgolini")
+                // Trobem la posicio amb lastIndexOf
+                posicioRepetit_last = llistatPokemons.lastIndexOf(llistatPokemons[i])
 
-        // Reemplaçem el cotxe per Francesco Virgolini i guardem al altre cotxe en la antiga posicio de Francesco.
-        cotxe_reemplazar = llistatCotxes[posicioFrancesco - 1]
-        llistatCotxes[posicioFrancesco - 1] = "Francesco Virgolini"
-        llistatCotxes[posicioFrancesco] = cotxe_reemplazar
+                // Comprovem si la posicio no es la mateixa que estem comparant.
+                if (posicioRepetit_last != i) {
 
-        // Imprimim el resultat.
-        println(llistatCotxes)
+                    // Comparem els nivels dels dos pokemon i posem a 0 el mes baix.
+                    if (llistatNivells[i] > llistatNivells[posicioRepetit_last]) {
+                        llistatPokemons[posicioRepetit_last] = 0
 
+                    } else {
+                        llistatPokemons[i] = 0
+                        posicioRepetitFirst = llistatPokemons.indexOf(llistatPokemons[posicioRepetit_last])
+
+                        //Mirem si el element torna a apareixer
+                        if (posicioRepetitFirst != -1) {
+
+                            //mirem si no es la mateixa posicio
+                            if (posicioRepetitFirst != posicioRepetit_last) {
+
+                                //Comparem i posem a 0 el mes petit.
+                                if (llistatPokemons[posicioRepetitFirst] in llistatPokemons) {
+                                    posicioRepetit_last =
+                                        llistatPokemons.lastIndexOf(llistatPokemons[posicioRepetitFirst])
+                                    if (llistatNivells[posicioRepetitFirst] > llistatNivells[posicioRepetit_last]) {
+                                        llistatPokemons[posicioRepetit_last] = 0
+                                    } else {
+                                        llistatPokemons[posicioRepetitFirst] = 0
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // Afegim cada element que no sigui 0 a la matriu nova.
+        for (posicio in llistatPokemons.indices) {
+            if (llistatPokemons[posicio] != 0) {
+                llistatPokemons_Nou.add(llistatPokemons[posicio])
+            }
+        }
+
+        // Imprimim el nou llistat.
+        for (element in llistatPokemons_Nou) {
+            print("$element ")
+        }
+        println()
     }
+
     scan.close()
 }
