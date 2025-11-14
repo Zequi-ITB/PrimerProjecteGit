3bd44ccb09987c027deb79125c47bbae6fdeba1e
zequi-itb <ezequiel.carracedo.7e9@itb.cat>
2025-11-14 10:40:27 +0100

    14/11

--- src/joEl/joEl2.kt
+++ src/joEl/joEl2.kt
@@ -1,39 +1,40 @@
+import java.util.Locale
 import java.util.Scanner
+import kotlin.math.PI
 
-//L'altre dia vaig anar al Mercadona a comprar vodka Knebep. I una dona se'm va colar!!!! Intolerable.
-//Per cada cas de prova es vol retornar tota la llista de nombres propocionada, pero amb l'element C que s'ha colat a la posició P
-//Es considera que la primera posició és 0.
+//En una mesa redonda no hay ningun lugar destacado y, por lo tanto, nadie ocupa una posición de privilegio.
+//Por esta razón, el sabio rey Arturo eligió una mesa redonda para reunir a sus caballeros. De este modo,
+//los caballeros eran todos iguales, no había «líderes» y se ahorraba alguna revuelta.
+//Pero más que las revueltas, lo que realmente le daba miedo al rey Arturo eran las acaloradas argumentaciones que
+//hacían los caballeros. En más de una ocasión habían sacado la espada y la habían batido al viento de un lado a otro
+//mientras los caballeros de los lados procuraban no salir heridos.
+//Por eso, el rey Arturo te pide tu ayuda. Ha calculado el espacio necesario del perímetro de la tabla que tiene que
+//tener un caballero para él solo, incluyendo el margen de seguridad, para no hacer daño a nadie. De este modo, todos
+//los caballeros se encontrarán cómodos y podrán defender libremente sus posturas. Hace falta que le digas al rey Arturo
+//si, dada una mesa y el espacio que le hace falta a cada caballero, caben todos o no.
 
+//Hay que indicar "YES" (dado que rey Arthur era inglés) si caben todos los caballeros cómodamente o "NO" en caso contrario.
 
 fun main() {
-    val scan: Scanner = Scanner(System.`in`)
+    val scan: Scanner = Scanner(System.`in`).useLocale(Locale.UK)
 
-    // Demanem la quantitat de casos
-    var casos: Int = scan.nextInt()
+    // Declarem a PI com una constant
+    val PI = PI
 
+    // Demanem el diametro de la taula, el espai minim de cada caballero i i la quantitat de caballeros.
+    var diametro: Double = scan.nextDouble()
+    var espai_seguretat: Double = scan.nextDouble()
+    var quantitat_caballeros: Double = scan.nextDouble()
 
-    // Fem un bucle amb la quantitat de casos
-    repeat(casos) {
-
-        //Demanem el tamany de la llista
-        var tamany: Int = scan.nextInt()
-
-        //Declarem el llistat i llegim els valors per teclat
-        var llistatCuaDeGent: MutableList<Int> = MutableList<Int>(tamany) { scan.nextInt() }
-
-        // Demanem el numero que es vol colar i la posicio a la que es cola.
-        var elementColat: Int = scan.nextInt()
-        var posicioColat: Int = scan.nextInt()
-
-        //Afeigim el element a la posicio desitjada.
-        llistatCuaDeGent.add(posicioColat, elementColat)
-
-        //Imprimim el resultat.
-        for (i in llistatCuaDeGent) {
-            print("$i ")
-        }
-        println()
+    //Comprovem si hi caben calculant els metres de la circunferencia (diametro*PI) i el espai de seguretat multiplicat per els caballers.
+    //Imprimim el resultat en cada cas.
+    if (diametro* kotlin.math.PI >= espai_seguretat*quantitat_caballeros ) {
+        println("YES")
     }
+    else {
+        println("NO")
+    }
+
 
     scan.close()
 }
