555319d2815760f2f03bc91e583b325e2740a038
ezequiel.carracedo.7e9 <ezequiel.carracedo.7e9@itb.cat>
2025-11-18 10:44:25 +0100

    18/11

--- src/joEl/kotlin.kt
+++ src/joEl/kotlin.kt
@@ -1,88 +1,70 @@
+import java.util.Locale
 import java.util.Scanner
 
-//El Joc de la Vida és un autòmata cel·lular dissenyat pel matemàtic britànic John Horton Conway al 1970
-//És un joc de zero jugadors, el que vol dir que la seva evolució està determinada pel seu estat inicial i no necessita
-// cap entrada de dades posterior. El tauler de joc és espai bidimensional rectangular, dividit en caselles (cèl·lules).
-// Cada casella te fins a 8 caselles veïnes, les que estan al seu voltant. Les cèl·lules tenen dos estats: vives o mortes.
-// Es tracta d’anar evolucionant el tauler de joc en base a unes regles, que marcaran si les caselles estan vives o mortes.
-// L’estat d’una casella evoluciona, d’una generació per la següent, sempre en funció de l’estat inicial de la matriu. en funció d’una sèrie de regles:
-//Una casella morta amb exactament 2 ó 3 caselles veïnes, “neix” (és a dir, passa a estar viva). Altrament roman morta.
-//Una casella viva amb 2 ó 3 caselles veïnes segueix viva, altrament mor o roman morta (per soledat, o superpoblació).
+//Definim la data class producte
+data class Alumne(
+    var nomAlumne: String,
+    var notaPrimerParcial: Float,
+    var notaSegonParcial: Float,
+    var notaFinal: Float
+)
 
-//Per cada cas de prova, caldrà respondre:
-//Una línia per cada fila, amb l’estat de cada columna a la següent generació, representat de la manera ja explicada i havent aplicat les regles del “Joc de la Vida”.
+//Passar les notes és una de les tasques més avorrides que pot tenir un professor. T'ofereixes a ajudar al professor
+//a passar les notes? Potser pots ficar-hi cullerada i arreglar-te les notes...
+
+//Cal mostrar 2 línies, amb el nom de l'alumne, la nota del primer parcial, la nota del segon parcial i la nota final, separades per un espai.
+//La primera linia mostrarà el nom de la persona que ocupa el lloc P1 segons el segon parcial.
+//La segona linia mostrarà el nom de la persona que ocupa el lloc P2 segons la seva nota final.
 
 fun main() {
-    val scan: Scanner = Scanner(System.`in`)
+    val scan: Scanner = Scanner(System.`in`).useLocale(Locale.UK)
 
-    //Demanem a l'usuari les dimensions i tamany de la matriu
-    var filas: Int = scan.nextInt()
-    var columnes: Int = scan.nextInt()
+    //Demanem a l'usuari la quanitat de productes que volem evaluar
+    var quantitatAlumnes: Int = scan.nextInt()
     scan.nextLine()
 
-    // Fem la matriu de Strings
-    var matriu: MutableList<String> = mutableListOf()
+    // Definim el llistat on guardarem tots els productes
+    var llistatAlumnes: MutableList<Alumne> = mutableListOf<Alumne>()
 
-    // Fem un bucle per afeigir les llistes a la matriu, llegim les dades com string
-    repeat(filas) {
-        var llistat: String = scan.nextLine()
-        matriu.add(llistat)
+    // Fem un bucle amb la quantitat de casos
+    repeat(quantitatAlumnes) {
+        //Definim la variable que farem servir per guardar els valors del producte
+        var alumne_: Alumne = Alumne("", 0.0f, 0.0f, 0.0f)
+
+        //Demanem les dades del producte
+        alumne_.nomAlumne = scan.next()
+        alumne_.notaPrimerParcial = scan.nextFloat()
+        alumne_.notaSegonParcial = scan.nextFloat()
+
+        //afegim cada producte a la llista
+        llistatAlumnes.add(alumne_)
+    }
+    scan.nextLine()
+
+    //Demanem el index del producte que volem consultar
+    var posicio1 = scan.nextInt()
+    var posicio2 = scan.nextInt()
+
+    //Fem una iteració per calcular la nota final de tots els elements.
+    for (i in llistatAlumnes.indices) {
+        llistatAlumnes[i].notaFinal =
+            llistatAlumnes[i].notaPrimerParcial * 40 / 100 + llistatAlumnes[i].notaSegonParcial * 60 / 100
     }
 
-    // Definim la nova matriu (de Chars) i initzialitzem tots els valors a '0'
-    var matriuNovaGeneracio = MutableList(filas) { MutableList(columnes) { '0' } }
+    //Ordenem la llista en ordre descendent segons el segon parcial i segons la nota final
+    var llistatOrdreDescParcial = llistatAlumnes.sortedBy { it.notaSegonParcial }.reversed()
+    var llistaOrdreDescNotaFinal = llistatAlumnes.sortedBy { it.notaFinal }.reversed()
 
-    //Declarem variables per comptabilitzar els 1 que hi ha al voltant.
-    var contadorCelulas: Int = 0
-    var fila_comprovar: Int = 0
-    var columna_comprovar: Int = 0
+    //Imprimim el resultat
+    println(
+        llistatOrdreDescParcial[posicio1 - 1].nomAlumne + " " + llistatOrdreDescParcial[posicio1 - 1].notaPrimerParcial + " " + llistatOrdreDescParcial[posicio1 - 1].notaSegonParcial + "  ${
+            "%2f".format(
+                llistatOrdreDescParcial[posicio1 - 1].notaFinal
+            )
+        }"
+    )
+    println(llistaOrdreDescNotaFinal[posicio2 - 1].nomAlumne + " " + llistaOrdreDescNotaFinal[posicio2 - 1].notaPrimerParcial + " " + llistaOrdreDescNotaFinal[posicio2 - 1].notaSegonParcial + " " + llistaOrdreDescNotaFinal[posicio2 - 1].notaFinal)
 
-    // Fem un bucle per iterar per totes les posicions de la matriu
-    for (i in matriu.indices) {
-        for (posicio in matriu[i].indices) {
-
-            // Després de cada comprovacio, tornem el comptador a 0
-            contadorCelulas = 0
-
-            // Fem un bucle for per fer desplaçaments verticals i horitzontals.
-            for (desplazamentVertical in -1..1) {
-                for (desplazamentHoritzontal in -1..1) {
-
-                    // Comprovem que la posicio no sigui 0,0.
-                    if (desplazamentHoritzontal != 0 || desplazamentVertical != 0) {
-
-                        fila_comprovar = i + desplazamentVertical
-                        columna_comprovar = posicio + desplazamentHoritzontal
-
-                        // Comprovem si esta dins del rang de la matriu
-                        if (fila_comprovar in 0..filas - 1 && columna_comprovar in 0..columnes - 1) {
-
-                            // Comprovem si es una celula i el comptabilitzem
-                            if (matriu[fila_comprovar][columna_comprovar] == '1') {
-                                contadorCelulas++
-                            }
-                        }
-                    }
-                }
-            }
-
-            //Comprovem si hi ha 2 o 3 celula vives al voltant i modifiquem la matriu de la nova generació
-            if (contadorCelulas == 2 || contadorCelulas == 3) {
-                matriuNovaGeneracio[i][posicio] = '1'
-            } else {
-                matriuNovaGeneracio[i][posicio] = '0'
-            }
-        }
-    }
-
-    // Fem una ultima iteracio per imprimir tots els elements de la matriu.
-    for (i in matriuNovaGeneracio.indices) {
-        for (posicio in matriuNovaGeneracio[i].indices) {
-            print(matriuNovaGeneracio[i][posicio])
-        }
-        println()
-
-    }
 
     scan.close()
 }
